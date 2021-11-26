1fb79a76315bfa87d5fb02875c295d7e68519b15
Danilo <didoherculano@gmail.com>
2021-11-26 01:05:52 -0300

    página search

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,10 +1,113 @@
-import React from 'react';
-import './search.css'
+import React, {useState, useEffect} from 'react';
 
+//---Styles---
+import './search.scss'
+
+//---Assets---
+import searchIcon from '../../assets/search.png'
+
+//---Services---
+import api from '../../services/api'
+
+//---Components---
+import Parameter from './parameter/Parameter';
+import PhotosList from '../../components/photoslist/PhotosList'
+
+//Initial state function to query
+function initialState(){
+    return {
+        text: '' ,
+        orientation: '' ,
+        size: '' ,
+        color: ''
+    }
+}
 function Search() {
+    const [query, setQuery] = useState(initialState)
+    const [photos, setPhotos] = useState()
+    console.log(photos)
+    console.log(query)
+
+    //Submit
+    function onSubmit(e){
+        e.preventDefault();
+
+        //Search photos
+        if(query.text !== ''){
+            api.photos.search({
+                query: query.text, 
+                per_page: 20, 
+                orientation: query.orientation, 
+                size: query.size, 
+                color: query.color
+            }).then(photos => {
+                setPhotos(photos) 
+            });
+        }
+    }
+
+    //Listening parameter change
+    useEffect(() => {
+        if(query.text !== ''){
+            api.photos.search({
+                query: query.text, 
+                per_page: 20, 
+                orientation: query.orientation, 
+                size: query.size, 
+                color: query.color
+            }).then(photos => {
+                setPhotos(photos) 
+            });
+        }
+    }, [query.orientation,query.size,query.color])
+
+
+    function getOrientation(option){
+        if(option !== 'All')
+            setQuery({...query, orientation: option})
+    }
+
+    function getSize(option){
+        if(option !== 'All')
+            setQuery({...query, size: option})
+    }
+
+    function getColor(option){
+        if(option !== 'All')
+            setQuery({...query, color: option})
+    }
+
     return ( 
         <div className="search">
-            Search
+            <form onSubmit={onSubmit}>
+                <div className="search-bar">
+                    <img src={searchIcon} alt="loading..." />
+                    <input 
+                        type="text" 
+                        value={query.text} 
+                        onChange={ e => setQuery({...query, text: e.target.value})}
+                        placeholder='Search nature, art, universe, etc.'
+                    />
+                </div>
+                <div className="parameters">
+                    <Parameter 
+                        title='Orientation' 
+                        options={['Landscape','Portrait','Square','All']}
+                        getOption={getOrientation}
+                    />
+                    <Parameter 
+                        title='Size' 
+                        options={['Large','Medium','Small','All']}
+                        getOption={getSize}
+                    />
+                    <Parameter 
+                        title='Color' 
+                        options={['All','Red','Orange','Yellow','Green','Turquoise','Blue','Pink','Brown','Black','Gray','White']}
+                        getOption={getColor}
+                    />
+                </div>
+            </form>
+            <PhotosList photos={photos}/>
         </div>
      );
 }
---
+++ src/pages/search/parameter/Parameter.jsx
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+
+//---Styles---
+import './parameter.scss'
+
+//---Assets---
+import noneIcon from '../../../assets/none.png'
+
+function Parameter(props) {
+    const [optionSelected, setOptionSelected] = useState()
+
+    function optionClick(option){
+        setOptionSelected(option)
+        props.getOption(option.toLowerCase())
+    }
+
+    return ( 
+        <div className='parameter'>
+            <div className="title">{optionSelected ? optionSelected : props.title}</div>
+            <div className={`options ${props.title}`}>
+                {props.options.map( (option,index) => {
+                    if(props.title === 'Color'){
+                        if(option === 'All')
+                            return <img src={noneIcon} alt="loading..." className="color" onClick={ () => optionClick(option)} style={{boxShadow: 'none'}}/>
+                        else
+                            return <div key={index} className="color" style={{background: option}} onClick={ () => optionClick(option) }/>                         
+                    }
+                    else{
+                        return <li key={index} onClick={ () => optionClick(option) }> {option} </li> 
+                    }
+                })}
+            </div>
+        </div>
+     );
+}
+
+export default Parameter
